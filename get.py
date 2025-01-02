4126d8ee1954d66ee680dff3b2afe75db5b6e2a4
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2025-01-03 02:35:24 +0300

    feat(stock-data): Successfully fetched US stock data via YFinance API  US stock prices were successfully retrieved using the YFinance API. Integration for Istanbul Stock Exchange (BIST) data will be implemented next.

--- get.py
+++ get.py
@@ -15,38 +15,77 @@ def home():
     
     
     """ coin url """
-    symbols = ["BTC-USD", "ETH-USD", "XRP-USD"]
+    symbols = [
+       "BTC-USD", "ETH-USD", "XRP-USD", "ADA-USD", "SOL-USD",
+    "DOGE-USD", "DOT-USD", "BNB-USD", "LTC-USD", "MATIC-USD",
+    "SHIB-USD", "AVAX-USD", "UNI-USD", "TRX-USD", "XLM-USD",
+    "ATOM-USD", "LINK-USD", "FTT-USD", "ALGO-USD", "ICP-USD",
+    
+
+   
+]
+
     crepto_dt=[]
     for symbol in symbols:
       
 
       tce=yf.Ticker(symbol)
-      a=tce.history(period="1mo")
+      a=tce.history(period="1d")
 
       b=a["Close"].iloc[-1]
       crepto_dt.append({"name": symbol , "price": b})
 
     return render_template("hm.html", crepto=crepto_dt)
       
-    
+ 
 
 
 @app.route("/stock")
 def stok():
-   usstocks = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "META", "NVDA", "BRK.B", "JPM", "V"]
+   usstocks = ["AAPL", "MSFT", "GOOGL","AMZN", "TSLA", "META", "NVDA","META", "NVDA", "NFLX",  
+    "AMD",   
+    "INTC",  
+    "ORCL",  
+    "DIS",  
+    "BA",    
+    "NKE", 
+    "IBM",
+    "CSCO",  
+    "CMCSA", 
+    "PYPL",  
+    "ADBE", 
+    "PEP",   
+    "COST",  
+    "KO",    
+    "MCD",   
+    "QCOM",  
+    "CVX",   
+    "T",   
+    "VZ",    
+    "XOM",   
+    "SPGI",  
+    "GS",    
+    "JPM",   
+    "WMT",   
+    "TGT",   
+    "UNH",   
+    "PFE",   
+    "MRK"
+        ]
+  
+
    us=[]
 
-   for abdstock in usstocks: 
+   for abdstock  in usstocks : 
+
+  
      st=yf.Ticker(abdstock)
-     hstr=st.history(period="1mo")
-     end=hstr["Close"].iloc[-1]
-     if hstr.empty:
-        us.append("hata oluştu")
-        continue
-        
-     
+     hstr=st.history(period="1d")
+     end=hstr["Close"].iloc[-1]   
      us.append({"ad": abdstock , "fıyat": end })
    return render_template("stock.html", uasto=us )
+
+
 
 
 if __name__ == "__main__":
