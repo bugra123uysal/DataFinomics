49cd6b1126819f8fcbe07cf83a0e0901706b4efb
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2025-01-07 02:25:33 +0300

    The project has started in the web section, where the graphics section will be added later, and the My Portfolio section will be added in its place.

--- get.py
+++ get.py
@@ -9,6 +9,12 @@ from flask import Flask, render_template
 import  json
 import io
 import base64
+import os
+
+static_dir=os.path.join(os.path.dirname(__file__), 'templates')
+if not os.path.exists(static_dir):
+   os.makedirs(static_dir)
+
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")  
@@ -16,17 +22,17 @@ def home():
     
     
     """ coin url """
+    
     symbols = [
-       "BTC-USD", "ETH-USD", "XRP-USD", "ADA-USD", "SOL-USD",
+    "BTC-USD", "ETH-USD","XRP-USD", "ADA-USD", "SOL-USD",
     "DOGE-USD", "DOT-USD", "BNB-USD", "LTC-USD", "MATIC-USD",
     "SHIB-USD", "AVAX-USD", "UNI-USD", "TRX-USD", "XLM-USD",
-    "ATOM-USD", "LINK-USD", "FTT-USD", "ALGO-USD", "ICP-USD",
-    
-
-   
-]
-
+    "ATOM-USD", "LINK-USD", "FTT-USD", "ALGO-USD", "ICP-USD"]
     crepto_dt=[]
+
+
+
+
     for symbol in symbols:
       
 
@@ -34,66 +40,30 @@ def home():
       a=tce.history(period="1d")
 
       b=a["Close"].iloc[-1]
-      crepto_dt.append({"name": symbol , "price": b})
-      
-       
       hh=yf.download(symbol , start="2024-01-01" , end="2025-01-04")
       plt.plot(hh.index , hh["Close"]  )
       plt.title("coin name: "+symbol )
       plt.grid(True)
-      plt.show()
+      plt.legend()
+      
+      crepto_dt.append({"name": symbol , "price": b  })
+      
      
     return render_template("hm.html", crepto=crepto_dt)
 
 
 @app.route("/stock")
 def stok():
-   usstocks = ["AAPL", "MSFT", "GOOGL","AMZN", "TSLA", "META", "NVDA","META", "NVDA", "NFLX",  
-    "AMD",   
-    "INTC",  
-    "ORCL",  
-    "DIS",  
-    "BA",    
-    "NKE", 
-    "IBM",
-    "CSCO",  
-    "CMCSA", 
-    "PYPL",  
-    "ADBE", 
-    "PEP",   
-    "COST",  
-    "KO",    
-    "MCD",   
-    "QCOM",  
-    "CVX",   
-    "T",   
-    "VZ",    
-    "XOM",   
-    "SPGI",  
-    "GS",    
-    "JPM",   
-    "WMT",   
-    "TGT",   
-    "UNH",   
-    "PFE",   
-    "MRK"
-        ]
-  
-
+   usstocks = ["AAPL", "MSFT", "GOOGL","AMZN", "TSLA", "META", "NVDA","META", "NVDA", "NFLX",  "AMD",   "INTC",  "ORCL",  "DIS",  "BA",    "NKE", "IBM","CSCO",  "CMCSA", "PYPL",  "ADBE", "PEP",   "COST",  "KO",    "MCD",   "QCOM",  "CVX",   "T",   "VZ",    "XOM",   "SPGI",  "GS",    "JPM",   "WMT",   "TGT",   "UNH",   "PFE","MRK" ]
    us=[]
 
    for abdstock  in usstocks : 
-
   
      st=yf.Ticker(abdstock)
      hstr=st.history(period="1d")
      end=hstr["Close"].iloc[-1]   
      us.append({"ad": abdstock , "fıyat": end })
    return render_template("stock.html", uasto=us )
-
-
-
-
 
 
 if __name__ == "__main__":
