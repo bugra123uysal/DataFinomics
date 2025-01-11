9366d3e0519bee7b8cd217f6d93004d07fe5100b
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2025-01-11 04:39:04 +0300

    Update  I started working on keeping data up to date with threadin to keep stocks and cryptocurrencies up to date.

--- get.py
+++ get.py
@@ -10,6 +10,8 @@ import  json
 import io
 import base64
 import os
+import time
+import threading
 
 static_dir=os.path.join(os.path.dirname(__file__), 'templates')
 if not os.path.exists(static_dir):
@@ -21,13 +23,15 @@ app = Flask(__name__, template_folder="templates")
 def home():
     
     
-    """ coin url """
-    
-    symbols = [
-    "BTC-USD", "ETH-USD","XRP-USD", "ADA-USD", "SOL-USD",
+    """ coin url
+     "XRP-USD", "ADA-USD", "SOL-USD",
     "DOGE-USD", "DOT-USD", "BNB-USD", "LTC-USD", "MATIC-USD",
     "SHIB-USD", "AVAX-USD", "UNI-USD", "TRX-USD", "XLM-USD",
-    "ATOM-USD", "LINK-USD", "FTT-USD", "ALGO-USD", "ICP-USD"]
+    "ATOM-USD", "LINK-USD", "FTT-USD", "ALGO-USD", "ICP-USD"
+       """
+    
+    symbols = [
+    "BTC-USD", "ETH-USD"]
     crepto_dt=[]
 
 
@@ -37,6 +41,10 @@ def home():
       
 
       tce=yf.Ticker(symbol)
+      
+
+
+
       a=tce.history(period="1d")
 
       b=a["Close"].iloc[-1]
@@ -45,11 +53,15 @@ def home():
       plt.title("coin name: "+symbol )
       plt.grid(True)
       plt.legend()
-      
+
+      time.sleep(10)
+     
+
       crepto_dt.append({"name": symbol , "price": b  })
       
      
-    return render_template("hm.html", crepto=crepto_dt)
+    return render_template("hm.html"  ,  crepto=crepto_dt)
+
 
 
 @app.route("/stock")
@@ -63,7 +75,32 @@ def stok():
      hstr=st.history(period="1d")
      end=hstr["Close"].iloc[-1]   
      us.append({"ad": abdstock , "fıyat": end })
+   time.sleep(10)
    return render_template("stock.html", uasto=us )
+
+thread=threading.Thread(target=stok)
+thread.start()
+
+@app.route("/info")
+def inf():
+      
+      symbols = [
+    "BTC-USD", "ETH-USD"]
+      iinf=[]
+      
+      for sss in  symbols:
+       
+       """ info section """
+       ttt=yf.Ticker(sss)
+
+       iif=ttt.info
+       """ market capt """
+       mrecpt=iif.get("marketCap", 'N/A' )
+       """ hacmi """
+       hcm=iif.get("regularMarketVolume", 'N/A')
+ 
+       iinf.append({"name" : sss , "marketCap:" : mrecpt , "hacım": hcm })
+      return render_template("info.html",lk=iinf )
 
 
 if __name__ == "__main__":
